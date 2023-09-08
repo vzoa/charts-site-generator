980ffecb642e5f97b46eb5bfe971b88eba8a23c2
Ken Greim <34892440+kengreim@users.noreply.github.com>
2023-09-08 10:12:47 -0700

    Adding cloudflare pages build script

---
+++ build-cloudflare.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/sh
+curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
+chmod +x dotnet-install.sh
+./dotnet-install.sh -c 7.0 -InstallDir ./dotnet
+./dotnet/dotnet --version
+./dotnet/dotnet publish -c Release -o output
+./output/charts-site-generator /wwwroot
+npm install
+npx tailwindcss -i ./style.css -o ./output/wwwroot/style.css
+cp ./script.jss ./output/wwwroot
