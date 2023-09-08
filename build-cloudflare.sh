0ddacef57b1446f7f700fa49aac2e796e51179c8
Ken Greim <34892440+kengreim@users.noreply.github.com>
2023-09-08 10:45:44 -0700

    Update build-cloudflare.sh

--- build-cloudflare.sh
+++ build-cloudflare.sh
@@ -1,9 +1,9 @@
-﻿curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
+﻿#!/bin/sh
+curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 7.0 -InstallDir ./dotnet
 ./dotnet/dotnet --version
 ./dotnet/dotnet publish -c Release -o output
 ./output/charts-site-generator /wwwroot
-npm install
 npx tailwindcss -i ./style.css -o ./output/wwwroot/style.css
 cp ./script.js ./output/wwwroot
